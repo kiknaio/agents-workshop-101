4e1a2b81f436ab90d75dd0f7f630d58a426afbb5
George Kiknadze <g.kiknadze@hotmail.com>
2025-07-09 15:52:43 +0400

    update maxSteps and instructions

--- src/mastra/agents/github-agent.ts
+++ src/mastra/agents/github-agent.ts
@@ -7,14 +7,11 @@ import { getFileContent } from "../tools/getFileContent";
 import { getRepositoryCommits } from "../tools/getRepositoryCommits";
 import { getRepositoryPullRequests } from "../tools/getRepositoryPullRequests";
 import { getRepositoryStars } from "../tools/getRepositoryStars";
+import { instructions } from "./instructions";
 
 export const githubAgent = new Agent({
   name: "Github Agent",
-  instructions: `
-  You're a helpful Github assistant that help user to get information about github repositories.
-  - You can use getFilePaths tool to get all file paths from the GitHub user repository.
-  - You can use getFileContent tool to get the content of the file.
-  `,
+  instructions,
   model: anthropic("claude-3-7-sonnet-latest"),
   memory: new Memory({
     storage: new LibSQLStore({
@@ -27,5 +24,11 @@ export const githubAgent = new Agent({
     getRepositoryCommits,
     getRepositoryPullRequests,
     getRepositoryStars,
+  },
+  defaultGenerateOptions: {
+    maxSteps: 15, // Increase from default 5 to 15
+  },
+  defaultStreamOptions: {
+    maxSteps: 25, // Increase from default 5 to 15
   },
 });
